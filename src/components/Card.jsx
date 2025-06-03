462b155de0ecdb0cc6e788fc93f9f002dc876c10
Juan Sebastian <juan.sebastianri28@gmail.com>
2025-06-03 20:23:42 +0000

    Fix: Fix size of cards and fonts in kpi's panel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Card = ({title, kpi, icon, className = ""}) => {
     return (
         <div className={`flex flex-col justify-center items-center font-semibold text-white p-2 pr-4 pl-4 rounded-md ${className}`}>
-            <h3 className="heading-3 text-center">{title}</h3>
-            <div className="heading-1 font-bold flex gap-2 justify-center items-center">
+            <h3 className="heading-5 text-center">{title}</h3>
+            <div className="heading-3 font-bold flex gap-2 justify-center items-center">
                 {icon && <FontAwesomeIcon icon={icon} />}
                 {kpi}
             </div>
--- src/components/Paneles.jsx
+++ src/components/Paneles.jsx
@@ -1,5 +1,5 @@
 import Card from "./Card";
-import { faBuildingCircleCheck, faBuildingCircleExclamation, faBuildingCircleXmark, faPersonDigging } from "@fortawesome/free-solid-svg-icons";
+import { faBuildingCircleCheck, faBuildingCircleExclamation, faBuildingCircleXmark, faPersonDigging, faBriefcase } from "@fortawesome/free-solid-svg-icons";
 
 const Paneles = ({data}) => {
   const dataByEstado = data.reduce((acc, obra) => {
@@ -8,12 +8,21 @@ const Paneles = ({data}) => {
     return acc;
   }, {});
 
+  const totalContratos = data.reduce((acc, obra) => {
+    const contrato = obra.attributes.NUMERO_CON;
+    acc.add(contrato); 
+    return acc;
+  }, new Set());
+
+  const numeroDeContratos = totalContratos.size;
+
   return (
     <div className="flex justify-center gap-[10px] xl:gap-[20px] flex-wrap xl:flex-nowrap xl:justify-between">
-      <Card className="bg-dark-blue-400 w-[100%] xl:w-1/4" icon={faPersonDigging} title="Total Obras" kpi={data.length || 0} />
-      <Card className="bg-red-500 w-[calc(1/2*100%-10px)] xl:w-1/4" icon={faBuildingCircleXmark} title="Suspendidas" kpi={dataByEstado["Suspendido"] || 0} />
-      <Card className="bg-orange-500 w-[calc(1/2*100%-10px)] xl:w-1/4" icon={faBuildingCircleExclamation} title="En Ejecución" kpi={dataByEstado["En Ejecución"] || 0} />
-      <Card className="bg-green-500 w-[100%] xl:w-1/4" icon={faBuildingCircleCheck} title="Terminadas" kpi={dataByEstado["Terminado"] || 0} />
+      <Card className="bg-dark-blue-400 w-[100%] xl:w-1/5" icon={faPersonDigging} title="Total Obras" kpi={data.length || 0} />
+      <Card className="bg-red-500 w-[calc(1/2*100%-10px)] xl:w-1/5" icon={faBuildingCircleXmark} title="Suspendidas" kpi={dataByEstado["Suspendido"] || 0} />
+      <Card className="bg-orange-500 w-[calc(1/2*100%-10px)] xl:w-1/5" icon={faBuildingCircleExclamation} title="En Ejecución" kpi={dataByEstado["En Ejecución"] || 0} />
+      <Card className="bg-green-500 w-[100%] xl:w-1/5" icon={faBuildingCircleCheck} title="Terminadas" kpi={dataByEstado["Terminado"] || 0} />
+      <Card className="bg-dark-blue-500 w-[100%] xl:w-1/5" icon={faBriefcase} title="Total Contratos" kpi={numeroDeContratos || 0} />
     </div>
   );
 };
